4652e958caf06babf0db255ee8fe469596f98aee
Philipp Scheit <p.scheit@ps-webforge.com>
2015-12-13 23:21:24 +0100

    use main attributes for a lot of shimneys

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var amdWrap = require('gulp-wrap-amd');
+var rename = require('gulp-rename');
 
 gulp.task('superagent', function() {
   var execSync = require('child_process').execSync;
@@ -6,4 +8,15 @@ gulp.task('superagent', function() {
   var modulePath = path.dirname(require.resolve('superagent'));
 
   execSync('browserify --standalone superagent --outfile '+__dirname+'/shimneys/superagent/main.js .', {'cwd': modulePath});
+});
+
+gulp.task('amplify', function() {
+   return gulp.src(['resources/amplify/amplify.js'])
+     .pipe(amdWrap({
+        deps: ['jquery'],
+        params: ['jQuery'],
+        exports: 'amplify'
+      }))
+     .pipe(rename('main.js'))
+     .pipe(gulp.dest('shimneys/amplify'))
 });
